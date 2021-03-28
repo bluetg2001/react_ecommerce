9535dd7b3a220331575b5c061286d9df7194820b
bluetg2001 <bluetg2001@naver.com>
2021-03-29 08:09:17 +0900

    Header / Language 컴포넌트 수정 및 기능 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
-import Languages from './components/Languages/Languages';
 import { useSelector } from 'react-redux';
 import './css/index.css';
 
 function Header() {
   const menuSet = useSelector((state) => state.layouts.menus);
 
+  const [isEnClicked, setIsEnClicked] = useState(true);
+  const [isRuClicked, setIsRuClicked] = useState(false);
+
+  const selectEn = useCallback(() => {
+    setIsEnClicked(true);
+    setIsRuClicked(false);
+  }, []);
+
+  const selectRu = useCallback(() => {
+    setIsRuClicked(true);
+    setIsEnClicked(false);
+  }, []);
+
   return (
     <header className="header">
       <div className="wrapper">
         <Logo menus={menuSet} />
         <Navigation menus={menuSet} />
-        <Languages />
+        <ul className="languages">
+          <li
+            className={`language ${isEnClicked ? 'clicked' : 'none-click'}
+                  ${isRuClicked ? 'none-click' : 'clicked'}`}
+            onClick={selectEn}
+          >
+            EN
+          </li>
+          <li
+            className={`language ${isRuClicked ? 'clicked' : 'none-click'}
+                  ${isEnClicked ? 'none-click' : 'clicked'}`}
+            onClick={selectRu}
+          >
+            RU
+          </li>
+        </ul>
       </div>
     </header>
   );
